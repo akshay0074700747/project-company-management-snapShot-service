760c545095bcadbd87dfd948671cff9ac451919c
akshay <ask029849@gmail.com>
2024-03-22 13:05:54 +0530

    slight changes

--- internal/services/consumer.go
+++ internal/services/consumer.go
@@ -125,7 +125,7 @@ func (snap *SnapShotService) StartConsumerGroup() {
 	var consumer sarama.Consumer
 	var err error
 	for i := 0; i < 5; i++ {
-		consumer, err = sarama.NewConsumer([]string{"localhost:9092"}, config)
+		consumer, err = sarama.NewConsumer([]string{"host.docker.internal:9092"}, config)
 		if err != nil {
 			if i == 4 {
 				log.Fatal("Closingg: %v", err)
