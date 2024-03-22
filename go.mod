9584588181736c44e62c00349fa3210356cd1baf
akshay <ask029849@gmail.com>
2024-03-22 12:10:52 +0530

    slight changes

--- go.mod
+++ go.mod
@@ -2,32 +2,49 @@ module github.com/akshay0074700747/projectandCompany_management_snapShot-service
 
 go 1.21.5
 
-require github.com/minio/minio-go/v7 v7.0.67
+require (
+	github.com/minio/minio-go/v7 v7.0.67
+	go.mongodb.org/mongo-driver v1.14.0
+	google.golang.org/grpc v1.61.0
+)
 
 require (
+	github.com/IBM/sarama v1.43.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.6.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.14.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
-	google.golang.org/grpc v1.61.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
 
 require (
 	github.com/akshay0074700747/projectandCompany_management_protofiles v0.0.0-20240223082105-e44ee70d9b0d
-	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-chi/chi v1.5.5
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/minio/sha256-simd v1.0.1 // indirect
@@ -36,9 +53,9 @@ require (
 	github.com/rs/cors v1.10.1
 	github.com/rs/xid v1.5.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- internal/services/consumer.go
+++ internal/services/consumer.go
@@ -4,119 +4,179 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"log"
 
+	"github.com/IBM/sarama"
 	"github.com/akshay0074700747/projectandCompany_management_protofiles/pb/userpb"
 	"github.com/akshay0074700747/projectandCompany_management_snapShot-service/entities"
 	"github.com/akshay0074700747/projectandCompany_management_snapShot-service/helpers"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func (snap *SnapShotService) StartConsumerGroup() {
 
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers":        "host.docker.internal:9092",
-		"group.id":                 "snapshotConsumers",
-		"auto.offset.reset":        "earliest",
-		"enable.auto.commit":       "false",
-		"allow.auto.create.topics": true})
+	// consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+	// 	"bootstrap.servers":        "host.docker.internal:9092",
+	// 	"group.id":                 "snapshotConsumers",
+	// 	"auto.offset.reset":        "earliest",
+	// 	"enable.auto.commit":       "false",
+	// 	"allow.auto.create.topics": true})
+	// if err != nil {
+	// 	helpers.PrintErr(err, "error occured at creating a kafka consumer")
+	// 	return
+	// }
+
+	// topic := "SnapshotTopic"
+
+	// err = consumer.Assign([]kafka.TopicPartition{
+	// 	{
+	// 		Topic:     &topic,
+	// 		Partition: 0,
+	// 		Offset:    kafka.OffsetStored,
+	// 	},
+	// })
+	// if err != nil {
+	// 	helpers.PrintErr(err, "Error assigning partitions")
+	// 	return
+	// }
+
+	// sigchan := make(chan os.Signal, 1)
+	// signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	// defer func() {
+	// 	signal.Stop(sigchan)
+	// 	consumer.Close()
+	// }()
+
+	// run := true
+	// for run {
+	// 	select {
+	// 	case sig := <-sigchan:
+	// 		fmt.Printf("Received signal: %v\n", sig)
+	// 		run = false
+
+	// 	default:
+	// 		ev := consumer.Poll(1)
+	// 		if ev == nil {
+	// 			continue
+	// 		}
+
+	// 		switch e := ev.(type) {
+	// 		case *kafka.Message:
+	// 			fmt.Printf("Received message ")
+
+	// 			var msg entities.SnapMsg
+	// 			var meta entities.SnapMessage
+
+	// 			err := json.Unmarshal(e.Value, &msg)
+	// 			if err != nil {
+	// 				fmt.Printf("Error unmarshalling message value: %v\n", err)
+	// 				return
+	// 			}
+
+	// 			userID, err := snap.UserConn.GetByEmail(context.TODO(), &userpb.GetByEmailReq{
+	// 				Email: msg.Email,
+	// 			})
+	// 			if err != nil {
+	// 				fmt.Printf("Error retrieving userID from userservice", err)
+	// 				return
+	// 			}
+
+	// 			meta.UserID = userID.UserID
+	// 			meta.ProjectID = msg.ProjectID
+
+	// 			for _, snapShot := range msg.Snapshots {
+	// 				if err = snap.Usecase.InsertSnapShot(snapShot.Filename, snapShot.File, msg.Email, msg.ProjectID); err != nil {
+	// 					helpers.PrintErr(err, "Error happened at InsertSnapShot")
+	// 				}
+	// 				meta.Snapshots = append(meta.Snapshots, entities.Snapshot{
+	// 					Filename:    snapShot.Filename,
+	// 					Key:         snapShot.Key,
+	// 					Description: snapShot.Description,
+	// 					Progress:    snapShot.Progress,
+	// 				})
+	// 			}
+
+	// 			fmt.Println("=====hree=====", msg.IsStaged)
+
+	// 			if err = snap.Usecase.InsertMetaData(meta, msg.IsStaged, msg.Key); err != nil {
+	// 				helpers.PrintErr(err, "Error occured on InsertMetaData usecase")
+	// 				continue
+	// 			}
+
+	// 			_, err = consumer.CommitOffsets([]kafka.TopicPartition{e.TopicPartition})
+	// 			if err != nil {
+	// 				helpers.PrintErr(err, "Error committing offset")
+	// 			}
+	// 		case (kafka.Error):
+	// 			helpers.PrintErr(e, "errror occured at consumer")
+	// 		default:
+	// 			fmt.Printf("Ignored event: %v\n", e)
+
+	// 		}
+	// 	}
+	// }
+
+	// fmt.Println("Consumer shutting down...")
+
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Metadata.AllowAutoTopicCreation = true
+	config.Consumer.Offsets.AutoCommit.Enable = true
+	consumer, err := sarama.NewConsumer([]string{"host.docker.internal:9092"}, config)
 	if err != nil {
-		helpers.PrintErr(err, "error occured at creating a kafka consumer")
-		return
+		log.Fatalf("Error creating consumer: %v", err)
 	}
+	defer consumer.Close()
 
-	topic := "SnapshotTopic"
-
-	err = consumer.Assign([]kafka.TopicPartition{
-		{
-			Topic:     &topic,
-			Partition: 0,
-			Offset:    kafka.OffsetStored,
-		},
-	})
+	partitionConsumer, err := consumer.ConsumePartition("SnapshotTopic", 0, sarama.OffsetNewest)
 	if err != nil {
-		helpers.PrintErr(err, "Error assigning partitions")
-		return
+		log.Fatalf("Error creating partition consumer: %v", err)
 	}
+	defer partitionConsumer.Close()
 
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-	defer func() {
-		signal.Stop(sigchan)
-		consumer.Close()
-	}()
-
-	run := true
-	for run {
+	for {
 		select {
-		case sig := <-sigchan:
-			fmt.Printf("Received signal: %v\n", sig)
-			run = false
+		case e := <-partitionConsumer.Messages():
+			var msg entities.SnapMsg
+			var meta entities.SnapMessage
 
-		default:
-			ev := consumer.Poll(1)
-			if ev == nil {
+			err := json.Unmarshal(e.Value, &msg)
+			if err != nil {
+				fmt.Printf("Error unmarshalling message value: %v\n", err)
+				return
+			}
+
+			userID, err := snap.UserConn.GetByEmail(context.TODO(), &userpb.GetByEmailReq{
+				Email: msg.Email,
+			})
+			if err != nil {
+				fmt.Printf("Error retrieving userID from userservice", err)
+				return
+			}
+
+			meta.UserID = userID.UserID
+			meta.ProjectID = msg.ProjectID
+
+			for _, snapShot := range msg.Snapshots {
+				if err = snap.Usecase.InsertSnapShot(snapShot.Filename, snapShot.File, msg.Email, msg.ProjectID); err != nil {
+					helpers.PrintErr(err, "Error happened at InsertSnapShot")
+				}
+				meta.Snapshots = append(meta.Snapshots, entities.Snapshot{
+					Filename:    snapShot.Filename,
+					Key:         snapShot.Key,
+					Description: snapShot.Description,
+					Progress:    snapShot.Progress,
+				})
+			}
+
+			fmt.Println("=====hree=====", msg.IsStaged)
+
+			if err = snap.Usecase.InsertMetaData(meta, msg.IsStaged, msg.Key); err != nil {
+				helpers.PrintErr(err, "Error occured on InsertMetaData usecase")
 				continue
 			}
-
-			switch e := ev.(type) {
-			case *kafka.Message:
-				fmt.Printf("Received message ")
-
-				var msg entities.SnapMsg
-				var meta entities.SnapMessage
-
-				err := json.Unmarshal(e.Value, &msg)
-				if err != nil {
-					fmt.Printf("Error unmarshalling message value: %v\n", err)
-					return
-				}
-
-				userID, err := snap.UserConn.GetByEmail(context.TODO(), &userpb.GetByEmailReq{
-					Email: msg.Email,
-				})
-				if err != nil {
-					fmt.Printf("Error retrieving userID from userservice", err)
-					return
-				}
-
-				meta.UserID = userID.UserID
-				meta.ProjectID = msg.ProjectID
-
-				for _, snapShot := range msg.Snapshots {
-					if err = snap.Usecase.InsertSnapShot(snapShot.Filename, snapShot.File, msg.Email, msg.ProjectID); err != nil {
-						helpers.PrintErr(err, "Error happened at InsertSnapShot")
-					}
-					meta.Snapshots = append(meta.Snapshots, entities.Snapshot{
-						Filename:    snapShot.Filename,
-						Key:         snapShot.Key,
-						Description: snapShot.Description,
-						Progress:    snapShot.Progress,
-					})
-				}
-
-				fmt.Println("=====hree=====", msg.IsStaged)
-
-				if err = snap.Usecase.InsertMetaData(meta, msg.IsStaged, msg.Key); err != nil {
-					helpers.PrintErr(err, "Error occured on InsertMetaData usecase")
-					continue
-				}
-
-				_, err = consumer.CommitOffsets([]kafka.TopicPartition{e.TopicPartition})
-				if err != nil {
-					helpers.PrintErr(err, "Error committing offset")
-				}
-			case (kafka.Error):
-				helpers.PrintErr(e, "errror occured at consumer")
-			default:
-				fmt.Printf("Ignored event: %v\n", e)
-
-			}
+		case err := <-partitionConsumer.Errors():
+			log.Printf("Error consuming message: %v", err)
 		}
 	}
-
-	fmt.Println("Consumer shutting down...")
 
 }
