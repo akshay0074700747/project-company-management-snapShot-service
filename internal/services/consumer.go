89f9c5d634258beca05cccafeda684ac5a957a67
akshay <ask029849@gmail.com>
2024-03-22 12:37:21 +0530

    slight changes

--- internal/services/consumer.go
+++ internal/services/consumer.go
@@ -121,7 +121,7 @@ func (snap *SnapShotService) StartConsumerGroup() {
 	config.Consumer.Return.Errors = true
 	config.Metadata.AllowAutoTopicCreation = true
 	config.Consumer.Offsets.AutoCommit.Enable = true
-	consumer, err := sarama.NewConsumer([]string{"host.docker.internal:9092"}, config)
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, config)
 	if err != nil {
 		log.Fatalf("Error creating consumer: %v", err)
 	}
